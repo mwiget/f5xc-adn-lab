dbf7bcadeb5eb840a727b9a0ff805325b485d7b7
Marcel Wiget <m.wiget@f5.com>
2022-12-18 10:59:34 +0000

    dev and pre workloads working via consul

---
+++ services/variables.tf
@@ -0,0 +1,49 @@
+variable "project_prefix" {
+  type        = string
+  description = "Prefix added to names of all resources"
+  default     = "prefix"
+}
+
+variable "owner_tag" {
+  type        = string
+  default     = "m.wiget@f5.com"
+}
+
+variable "f5xc_api_url" {       
+  type = string
+}
+
+variable "f5xc_api_cert" {
+  type = string
+  default = ""
+}
+
+variable "f5xc_api_p12_file" {
+  type = string
+  default = ""
+}
+
+variable "f5xc_api_key" {
+  type = string
+  default = ""
+}
+
+variable "f5xc_api_ca_cert" {
+  type = string
+  default = ""
+}
+
+variable "f5xc_api_token" {
+  type = string
+  default = ""
+}
+
+variable "f5xc_tenant" {
+  type = string
+  default = ""
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
---
+++ services/workload-dev.tf
@@ -0,0 +1,198 @@
+resource "volterra_healthcheck" "workload_hc_dev" {
+  name      = format("%s-workload-hc", var.project_prefix)
+  namespace = format("%s-dev", var.project_prefix)
+
+  http_health_check {
+    use_origin_server_name = true
+    path                   = "/"
+  }
+  healthy_threshold   = 1
+  interval            = 15
+  timeout             = 1
+  unhealthy_threshold = 2
+}
+
+resource "volterra_origin_pool" "workload_dev" {
+  name                   = "workload-dev"
+  namespace              = format("%s-dev", var.project_prefix)
+  endpoint_selection     = "DISTRIBUTED"
+  loadbalancer_algorithm = "LB_OVERRIDE"
+  port                   = 8080
+  no_tls                 = true
+
+  origin_servers {
+    #    k8s_service {
+    #  service_name = "workload-dev"
+    #  site_locator {
+    #    virtual_site {
+    #      namespace = "shared"
+    #      name      = "mwadn-dev"
+    #    }
+    #  }
+    #}
+    consul_service {
+      service_name = "workload-dev"
+      site_locator {
+        virtual_site {
+          name      = "mwadn-dev"
+          namespace = "shared"
+        }
+      }
+      outside_network = true
+    }
+  }
+
+  healthcheck {
+    name = volterra_healthcheck.workload_hc_dev.name
+  }
+}
+
+resource "volterra_http_loadbalancer" "workload_dev" {
+  name                            = "workload-dev"
+  namespace                       = format("%s-dev", var.project_prefix)
+  no_challenge                    = true
+  domains                         = [ "workload-dev.mwadn-dev" ]
+
+  disable_rate_limit              = true
+  service_policies_from_namespace = true
+  disable_waf                     = true
+  source_ip_stickiness            = true
+
+  advertise_custom {
+    advertise_where {
+      port = 8080
+      site {
+        network = "SITE_NETWORK_INSIDE"
+        site {
+          name      = "marcel-zg01"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-us-east-2-a"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-us-east-2-b"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-us-east-2-c"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-us-east-2-d"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-us-west-2-a"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-us-west-2-b"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-us-west-2-c"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-eu-north-1-a"
+          namespace = "system"
+        }
+      }
+    }
+    advertise_where {
+      port = 8080
+      site {
+        ip = "10.10.10.10"
+        network = "SITE_NETWORK_OUTSIDE"
+        site {
+          name      = "mwadn-eu-north-1-b"
+          namespace = "system"
+        }
+      }
+    }
+  }
+
+  default_route_pools {
+    pool {
+      name = volterra_origin_pool.workload_dev.name
+    }
+    weight = 1
+    priority = 1
+  }
+
+  http {
+    dns_volterra_managed = false
+    port = 8080
+  }
+
+  depends_on = [ volterra_origin_pool.workload_dev ]
+}
+
+output "http_loadbalancer" {
+  value = resource.volterra_http_loadbalancer.workload_dev
+}
+output "origin_pool" {
+  value = resource.volterra_origin_pool.workload_dev
+}
+output "health_check_dev" {
+  value = resource.volterra_healthcheck.workload_hc_dev
+}
+
